3fdf57aeb9cbc08243856a5265e60949325ab83b
lgvv <lgw9898@naver.com>
2022-06-16 18:43:53 +0900

    [추가] Auth/SignUpRequest를 구현하였습니다.

---
+++ KaleubSDK/API/Auth/SignUpResponse.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpResponse.swift
+//  KaleubSDK
+//
+//  Created by Hamlit Jason on 2022/06/16.
+//
+
+import Foundation
+
+struct SignUpResponse: Response, Decodable {
+    typealias RequestType = SignUpRequest
+    
+    
+}
