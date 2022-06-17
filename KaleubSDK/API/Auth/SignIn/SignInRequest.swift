cecf39d9eecb108c077582613d95993e6b91e93a
lgvv <lgw9898@naver.com>
2022-06-17 19:06:08 +0900

    [추가] SignInRequest에 httpBody를 추가했습니다.

--- KaleubSDK/API/Auth/SignIn/SignInRequest.swift
+++ KaleubSDK/API/Auth/SignIn/SignInRequest.swift
@@ -23,6 +23,11 @@ struct SignInRequest: Request, Respondable {
 
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = method.stringValue
+        
+        let object = ["email": email, "password": password]
+        let data = try? JSONSerialization.data(withJSONObject: object, options: [])
+        urlRequest.httpBody = data
+        
         return urlRequest
     }
 }
