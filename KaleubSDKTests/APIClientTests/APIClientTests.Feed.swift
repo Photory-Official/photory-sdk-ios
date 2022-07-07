a5b3f30518e494f1282396bf38125af295c7b686
lgvv <lgw9898@naver.com>
2022-07-07 10:24:28 +0900

    [이슈] 이미지를 서버로 전송하기 위한 타입을 공부하고, [String]이 아닌 다른 타입으로 변경해야 합니다.

---
+++ KaleubSDKTests/APIClientTests/APIClientTests.Feed.swift
@@ -0,0 +1,38 @@
+//
+//  APIClientTests.Feed.swift
+//  KaleubSDKTests
+//
+//  Created by Hamlit Jason on 2022/07/07.
+//
+
+import XCTest
+@testable import KaleubSDK
+
+extension APIClientTests {
+    func test_createFeed() {
+        let expectation = XCTestExpectation()
+        expectation.expectedFulfillmentCount = 1
+        
+        let image: [String] = []
+        let roomId: Int64 = 1
+        let title: String = ""
+        let content: String = ""
+        
+        apiClient?.createFeed(image: image, roomId: roomId, title: title, content: content) { result in
+            switch result {
+            case .success:
+                print("✅")
+                XCTAssert(true)
+            case .failure(let error):
+                if let error = error as? APIClient.APIError {
+                    XCTFail(error.localizedDescription)
+                } else {
+                    XCTFail(error.localizedDescription)
+                }
+            }
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 30)
+    }
+}
