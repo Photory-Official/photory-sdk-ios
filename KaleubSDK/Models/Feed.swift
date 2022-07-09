ac69be417287ddd89a576689d576ad5cf8614cb0
lgvv <lgw9898@naver.com>
2022-07-09 15:04:07 +0900

    [수정] Model `Feed`를 수정했습니다.

--- KaleubSDK/Models/Feed.swift
+++ KaleubSDK/Models/Feed.swift
@@ -9,12 +9,8 @@ import Foundation
 
 public class Feed: Codable {
     
-    /// 방 생성시간
-    public var createdDate: Double
-    /// 방 수정시간
-    public var modifiedDate: Double
-    /// 해당 타입의 아이디
-    public var id: IdentifierType
+    /// 방 아이디
+    public var roomId: Int64
     /// 작성자 아이디
     public var userId: Int64
     /// 피드 제목
@@ -24,51 +20,35 @@ public class Feed: Codable {
     /// 피드 사진
     public var imageUrls: [String]
     
-    /// 해당하는 아이디의 타입.
-    public enum IdentifierType: Int64 {
-        /// 룸 아이디
-        case roomId
-        /// 피드 아이디
-        case feedId
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case createdDate = "createdAt"
-        case modifiedDate = "updatedAt"
-        case id
-        case code
-        case ownerEmail
-        case title
-        case password
-        case participantsCount
-    }
-    
     public init(
-        createdDate: Double,
-        modifiedDate: Double,
-        id: IdentifierType,
+        roomId: Int64,
         userId: Int64,
         title: String,
         content: String,
         imageUrls: [String]
     ) {
-        self.id = id
+        self.roomId = roomId
         self.userId = userId
         self.title = title
         self.content = content
         self.imageUrls = imageUrls
         // NOTE: - imageUrl 바로 UIImage로 바꿔버릴까??
-        self.createdDate = createdDate
-        self.modifiedDate = modifiedDate
     }
     
-    // TODO: - required 작성하기
+    enum CodingKeys: String, CodingKey {
+        case roomId
+        case userId
+        case title
+        case content
+        case imageUrls
+    }
+    
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.createdDate = try container.decode(Double.self, forKey: .createdDate)
-        self.modifiedDate = try container.decode(Double.self, forKey: .modifiedDate)
-        self.id = try container.decode(IdentifierType.self, forKey: .IdentifierType)
-        self.userId = try container.decode(Int64.self, forKey: .)
+        self.roomId = try container.decode(Int64.self, forKey: .roomId)
+        self.userId = try container.decode(Int64.self, forKey: .userId)
         self.title = try container.decode(String.self, forKey: .title)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.imageUrls = try container.decode([String].self, forKey: .imageUrls)
     }
 }
