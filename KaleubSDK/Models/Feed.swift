dc2297e46cb473d02264be95644726657adda3f8
lgvv <lgw9898@naver.com>
2022-07-09 14:50:50 +0900

    [기록] Feed의 구조를 잡았으나, CodingKeys 변환에 어려움을 겪어 변경합니다.

--- KaleubSDK/Models/Feed.swift
+++ KaleubSDK/Models/Feed.swift
@@ -13,8 +13,8 @@ public class Feed: Codable {
     public var createdDate: Double
     /// 방 수정시간
     public var modifiedDate: Double
-    /// 방 고유 아이디
-    public var roomId: Int64
+    /// 해당 타입의 아이디
+    public var id: IdentifierType
     /// 작성자 아이디
     public var userId: Int64
     /// 피드 제목
@@ -24,27 +24,35 @@ public class Feed: Codable {
     /// 피드 사진
     public var imageUrls: [String]
     
+    /// 해당하는 아이디의 타입.
+    public enum IdentifierType: Int64 {
+        /// 룸 아이디
+        case roomId
+        /// 피드 아이디
+        case feedId
+    }
+    
     enum CodingKeys: String, CodingKey {
+        case createdDate = "createdAt"
+        case modifiedDate = "updatedAt"
         case id
         case code
         case ownerEmail
         case title
         case password
         case participantsCount
-        case createdDate = "createdAt"
-        case modifiedDate = "updatedAt"
     }
     
     public init(
         createdDate: Double,
         modifiedDate: Double,
-        roomId: Int64,
+        id: IdentifierType,
         userId: Int64,
         title: String,
         content: String,
         imageUrls: [String]
     ) {
-        self.roomId = roomId
+        self.id = id
         self.userId = userId
         self.title = title
         self.content = content
@@ -55,4 +63,12 @@ public class Feed: Codable {
     }
     
     // TODO: - required 작성하기
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.createdDate = try container.decode(Double.self, forKey: .createdDate)
+        self.modifiedDate = try container.decode(Double.self, forKey: .modifiedDate)
+        self.id = try container.decode(IdentifierType.self, forKey: .IdentifierType)
+        self.userId = try container.decode(Int64.self, forKey: .)
+        self.title = try container.decode(String.self, forKey: .title)
+    }
 }
